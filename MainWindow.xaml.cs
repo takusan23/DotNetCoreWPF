e4c683c87f048bdc2ac78aa7d02102249d325919
takusan23 <takumu1200@gmail.com>
2020-06-09 01:15:51 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DotNetCoreWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // タイマー初期化
+            var dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+        }
+
+        // タイマーで毎秒ここ呼ばれる。
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            // 時間を表示
+            TimeTextBox.Text = DateTime.Now.ToString();
+        }
+
+    }
+}
